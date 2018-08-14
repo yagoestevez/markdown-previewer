9b4bc8e869cdb7807c98b6c74f5051d7710144fa
Yago Estevez <inbox@yagoestevez.com>
2018-08-14 14:16:15 +0200

    First commit

---
+++ src/App/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = props => {
+
+  return (
+    <header id="header">
+      <h1 className="title">Markdown Previewer</h1>
+      <p>menu</p>
+    </header>
+  );
+
+}
+
+export default Header;
